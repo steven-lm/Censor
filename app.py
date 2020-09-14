1c8f7add795a7a1d0552f4fbee8ba62a6b96c99e
stevenpcwin10 <steven.lam1@outlook.com>
2020-09-14 14:24:08 +1000

    added footer

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ import sys
 import time
 import uuid
 import requests
-from urllib.parse import urlparse
 from io import BytesIO
 from PIL import Image, ImageDraw, ImageFilter
 from azure.cognitiveservices.vision.face import FaceClient
